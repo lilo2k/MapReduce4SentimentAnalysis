8e1b780c28bac86dad49c7311a03c86cd58adfd3
Tommy Yoon <tdw9746@autuni.ac.nz>
2020-11-05 18:58:47 +1300

    init

---
+++ src/main/java/myproject/SentimentWordCount.java
@@ -0,0 +1,152 @@
+package myproject;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.regex.Pattern;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class SentimentWordCount {
+    /**
+     *
+     */
+    private static final String WORD_COUNT = "word count";
+    /**
+     *
+     */
+    private static final String COMPLETED_THE_MAP_REDUCE = "Completed the Map Reduce";
+    /**
+     *
+     */
+    private static final String EXITED_WITH_ERRORS = "Exited with Errors";
+    /**
+     *
+     */
+    private static final String COMPLETED_THE_MAP_REDUCE_SUCCESSFULLY = "Completed the Map Reduce Successfully";
+    /**
+     *
+     */
+    private static final String NON_ALPHABET = "[^a-zA-Z]";
+    
+    private static final String BRAND = "Alice";
+
+    // map
+    // Object = Input Key, Text = input Value, Text = Output of Map Process,
+    // IntWritable = Output Value = 1 Always <in this case> 1 . for each word
+    public static class TokenizeMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+        @Override
+
+        // we are getting a line of text at a time as input
+        // context is the way in which the key-value pairs is spit out.
+        public void map(Object inputKey, Text inputValue, Context context)
+                throws IOException, InterruptedException {
+
+            // pick up lines related to the BRAND
+            if (!Pattern.compile(Pattern.quote(BRAND), Pattern.CASE_INSENSITIVE).matcher(inputValue.toString()).find()) {
+                return;
+            }
+
+            IntWritable valueOne = new IntWritable(1);
+
+            StringTokenizer stringTokenizer = new StringTokenizer(inputValue.toString());
+
+            Text keyWord = new Text();
+
+            while (stringTokenizer.hasMoreTokens()) {
+                // removes all non alphabets, and any special characters
+                keyWord.set(stringTokenizer.nextToken().toLowerCase().replaceAll(NON_ALPHABET, ""));
+
+                context.write(keyWord, valueOne);
+            }
+        }
+    }
+
+    // reduce
+    // The input of the reducer is the output of the mapper.
+    public static class SumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+        // System automatically shuffle-sorts, and gives a iterable list as value to
+        // this reducer function.
+        // e.g. ("hello",1,1,1,1) hello is key, and 1,1,1,1 is iterable list
+        public void reduce(Text term, Iterable<IntWritable> listOfOnes, Context contextWriteAggregateOutput)
+                throws IOException, InterruptedException {
+            int count = 0;
+
+            for (IntWritable valueOne : listOfOnes) {
+                count++;
+            }
+            IntWritable output = new IntWritable(count);
+            contextWriteAggregateOutput.write(term, output);
+        }
+    }
+
+    // main
+    public static void main(String... args) throws IOException, ClassNotFoundException, InterruptedException {
+        // System.out.println( "Hello World!" );
+
+        Configuration conf = new Configuration();
+
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+
+        if (otherArgs.length != 2) {
+            System.err.println("* * * Needs two arguments....usage : WordCount <input_file> <output_folder>");
+            System.exit(2);
+        }
+
+        Job job = Job.getInstance(conf, WORD_COUNT);
+        job.setJarByClass(SentimentWordCount.class);
+
+        job.setMapperClass(TokenizeMapper.class);
+        job.setReducerClass(SumReducer.class);
+
+        job.setNumReduceTasks(2);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+
+        String outputFolderName = createOutputFolderName(args[1]);
+        FileOutputFormat.setOutputPath(job, new Path(outputFolderName));
+
+        boolean isSuccess_JobStatus = job.waitForCompletion(true); // submits the job 
+		
+		if(isSuccess_JobStatus) {
+			System.exit(0); // Exit with Success code	
+			System.out.println(COMPLETED_THE_MAP_REDUCE_SUCCESSFULLY);
+		}
+		else
+		{
+			System.exit(1); // Exit with Failure code # 1
+			System.out.println(EXITED_WITH_ERRORS);
+		}
+		
+		System.out.println(COMPLETED_THE_MAP_REDUCE);
+
+    }
+
+    private static String createOutputFolderName(String folderName) {
+
+        File file = new File(folderName);
+        if (file.exists()) {
+            return createOutputFolderName(folderName+"1");
+        }
+        return folderName;
+    }
+}
